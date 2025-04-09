ca746ca7fc1f8bc6f23fd0859b13f73101ba80eb
PacMan <nathan.nathanpark@gmail.com>
2025-04-09 10:17:10 +0900

    update

---
+++ js/pegasus-custom.js
@@ -0,0 +1,36 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PEGASUS CUSTOM JS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+
+	jQuery(document).ready(function($) {
+		
+		//if($(window).width() >= 768){
+			//initialize()
+		//}//end if
+		
+		//$( window ).resize(function() {
+			//if($(window).width() > 768){
+				//initialize();
+			//}
+			
+		//});
+
+	}); //end document ready function
+
+
+	jQuery(document).ready(function($) {
+		// executes when HTML-Document is loaded and DOM is ready
+		//alert("document is ready");
+	});
+
+
+	jQuery(window).on( 'load', function($) {
+		// executes when complete page is fully loaded, including all frames, objects and images
+		//alert("window is loaded");
+	});
